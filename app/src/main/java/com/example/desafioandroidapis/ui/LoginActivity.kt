ca3bb822a8935bc1f392b2ace5d5361621fe7a60
Rafael Santana <rafaelns.br@gmail.com>
2020-12-04 15:38:38 -0300

    Criando layout da página de login

--- app/src/main/java/com/example/desafioandroidapis/ui/LoginActivity.kt
+++ app/src/main/java/com/example/desafioandroidapis/ui/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.desafioandroidapis.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.desafioandroidapis.R
 
 class LoginActivity : AppCompatActivity() {
@@ -11,7 +12,8 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        startActivity(Intent(this, ComicDisplayActivity::class.java))
-        finish()
+        findViewById<Button>(R.id.btnLogin).setOnClickListener {
+            startActivity(Intent(this, ComicDisplayActivity::class.java))
+        }
     }
 }
